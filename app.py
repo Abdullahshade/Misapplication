a9526f711b0d0c83a05133c8bf907d58229a65be
ABDALLAH ALABDELAZIZ <129231021+Abdullahshade@users.noreply.github.com>
2024-11-26 01:02:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ except Exception as e:
     st.stop()
 
 # App title
-st.title("Pneumothorax Grading")
+st.title("Pneumothorax Viewer with Existing Grading")
 
 # Initialize session state for the current index
 if "current_index" not in st.session_state:
